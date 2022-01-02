d4f39d8c2720cec53f99dc9ffb93829648fe7f4c
Keagan Landfried <64995253+Zennara@users.noreply.github.com>
2022-01-02 03:06:03 +0000

    on_guild_join() and pre-built commands

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ import requests #to check discord api for limits/bans
 from replit import db #database storage
 
 #api limit checker
+#rate limits occur when you access the api too much. You can view Discord.py's api below. There it will tell you whether an action will access the api.
+#https://discordpy.readthedocs.io/en/stable/api.html
 r = requests.head(url="https://discord.com/api/v1")
 try:
   print(f"You are being Rate Limited : {int(r.headers['Retry-After']) / 60} minutes left")
@@ -21,6 +23,7 @@ except:
 #declare client
 intents = discord.Intents.all() #declare what Intents you use, these will be checked in the Discord dev portal
 client = discord.Client(intents=intents)
+
 
 @client.event
 async def on_ready():
@@ -32,6 +35,39 @@ async def on_message(message):
   #check for bots
   if message.author.bot:
     return
+
+  #convert the message to all lowercase
+  messagecontent = message.content.lower()
+
+  #this will clear the database if something is broken, WARNING: will delete all entries
+  if messagecontent == "z/clear":
+    #my database entries are seperates by server id for each key. this works MOST of the time unless you have a large amount of data
+    db[str(message.guild.id)] = {"prefix": "z/"}
+
+  #get prefix
+  prefix = db[str(message.guild.id)]["prefix"]
+
+  #this is to dump your databse into database.json. Change this to FALSE to stop this.
+  DUMP = True
+  if DUMP:
+    data2 = {}
+    count = 0
+    for key in db.keys():
+      data2[str(key)] = db[str(key)]
+      count += 1
+
+    with open("database.json", 'w') as f:
+      json.dump(str(data2), f)
+
+  #write your commands below!
+  #example command:
+    if messagecontent == prefix+"ping":
+      await message.channel.send("pong")
+
+@client.event
+async def on_guild_join(guild):
+  db[str(guild.id)] = {"prefix": "i/"} #for database support
+
 
 keep_alive.keep_alive() 
 #keep the bot running after the window closes, use UptimeRobot to ping the website at least every <60min. to prevent the website from going to sleep, turning off the bot
